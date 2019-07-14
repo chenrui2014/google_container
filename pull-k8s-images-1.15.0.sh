c8c4ae108fe41906a6fb765bc6861ffca9617642
chenrui2014 <chenruixiang2009@163.com>
2019-07-14 18:29:24 +0800

    k8s-1.15.0

--- pull-k8s-images-1.15.0.sh
+++ pull-k8s-images-1.15.0.sh
@@ -13,13 +13,14 @@
 set -e
 
 #运行kubeadm config images list确认指定版本
-K8S_VERSION=v1.13.2
-ETCD_VERSION=3.2.24
+K8S_VERSION=v1.15.0
+ETCD_VERSION=3.3.10
 DASHBOARD_VERSION=v1.10.0
-FLANNEL_VERSION=v0.11
-DNS_VERSION=1.2.6
+FLANNEL_VERSION=v0.11.0-amd64
+DNS_VERSION=1.3.1
 PAUSE_VERSION=3.1
-
+KUBE_CROSS_VERSION=v1.11.2-1
+DEBIAN_BASE_VERSION=V1.0.0
 ## 拉取images
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$K8S_VERSION
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$K8S_VERSION
@@ -28,8 +29,9 @@ docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:$ETCD_VERSION
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:$PAUSE_VERSION
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$DNS_VERSION
-docker pull registry.cn-hangzhou.aliyuncs.com/kubernetes_containers/flannel:$FLANNEL_VERSION
 docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:$DASHBOARD_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/kube-cross:$KUBE_CROSS_VERSION
+docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/debian-base-amd64:$DEBIAN_BASE_VERSION
 
 ## 修改tag
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$K8S_VERSION k8s.gcr.io/kube-apiserver-amd64:$K8S_VERSION
@@ -39,9 +41,9 @@ docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:$ETCD_VERSION k8s.gcr.io/etcd-amd64:$ETCD_VERSION
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:$PAUSE_VERSION k8s.gcr.io/pause-amd64:$PAUSE_VERSION
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$DNS_VERSION k8s.gcr.io/coredns:$DNS_VERSION
-docker tag registry.cn-hangzhou.aliyuncs.com/kubernetes_containers/flannel:$FLANNEL_VERSION quay.io/coreos/flannel:$FLANNEL_VERSION
 docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:$DASHBOARD_VERSION k8s.gcr.io/kubernetes-dashboard-amd64:$DASHBOARD_VERSION
-
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/kube-cross:$KUBE_CROSS_VERSION k8s.gcr.io/kube-cross:$KUBE_CROSS_VERSION
+docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/debian-base-amd64:$DEBIAN_BASE_VERSION k8s.gcr.io/debian-base-amd64:$DEBIAN_BASE_VERSION
 ## 删除原镜像
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-apiserver-amd64:$K8S_VERSION
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-controller-manager-amd64:$K8S_VERSION
@@ -50,5 +52,6 @@ docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-proxy-amd64:
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/etcd-amd64:$ETCD_VERSION
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/pause-amd64:$PAUSE_VERSION
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/coredns:$DNS_VERSION
-docker rmi registry.cn-hangzhou.aliyuncs.com/kubernetes_containers/flannel:$FLANNEL_VERSION
 docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kubernetes-dashboard-amd64:$DASHBOARD_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/kube-cross:$KUBE_CROSS_VERSION
+docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/debian-base-amd64:$DEBIAN_BASE_VERSION
